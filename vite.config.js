2907278df82089da7d25fdcd738e34e2c23b48ac
phil-on-air <phil.on.air@gmail.com>
2025-04-16 19:01:05 +0200

    Fix: Update paths to resolve correctly under /cosmic-spiral/

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: '/src/main.jsx'
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   },
